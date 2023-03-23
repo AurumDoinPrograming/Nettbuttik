5cc60972fcecd1dacfd7ea13418cf21c4874cf3b
AurumDoinPrograming <106434031+AurumDoinPrograming@users.noreply.github.com>
2023-03-23 13:00:27 +0100

    Add files via upload

---
+++ NettbutikkApp/NettbutikkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NettbutikkAppApp.swift
+//  NettbutikkApp
+//
+//  Created by Peder Nikolay Spongsveen on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NettbutikkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
